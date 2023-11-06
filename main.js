b208e6b3c45249d29564d6bc0b3e29153f8633e9
Jujuelv <107227558+Jujuelv@users.noreply.github.com>
2023-11-06 08:48:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert ('Ola mundo');
+function tocason (idELementoaudio){
+
+}
+
+const listaDeTeclas = document.querySelectorALL('.tecla');
+
+//para
+for (let contator =0;contador < listaDeTeclas.length; contador++) {
+
+    const tecla =listaDeTeclas[contador];
+    const instrumento =tecla.classlist[1];
+    const idAudio= 
+}
